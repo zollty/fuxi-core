4b9c3f3c65f68cd6d7a9dd47dfcd93ff7d8da396
zollty <zollty@163.com>
2024-03-11 20:33:27 +0800

    update

--- common_tests/sanic_test.py
+++ common_tests/sanic_test.py
@@ -3,35 +3,48 @@ from sanic_openapi import swagger_blueprint
 from sanic import Sanic
 from sanic.response import json
 
-app = Sanic(name="sdjkjdhszollty")
-app.config.API_VERSION = 'v0.0.1'
-app.config.API_TITLE = '异步平台 API文档'
-app.blueprint(swagger_blueprint)
+# app = Sanic(name="sdjkjdhszollty")
+# app.config.API_VERSION = 'v0.0.1'
+# app.config.API_TITLE = '异步平台 API文档'
+# app.blueprint(swagger_blueprint)
+#
+#
+# @app.get("/class", version=1)
+# @doc.summary("获取班级信息")  # API信息描述
+# @doc.tag("班级")  # API分组
+# @doc.consumes({"class_name": str}, location="query", required=False)
+# @doc.consumes({"id": int}, location="query", required=True)
+# async def get_class(request):
+#     return json({})
+#
+#
+# @app.get("/studentList", version=1)
+# @doc.summary("获取学生信息")
+# @doc.tag("学生")
+# @doc.consumes({"stu_name": str}, location="query", required=True)
+# async def get_student(request):
+#     return json({})
+#
+#
+# @app.post("/addStudent", version=2)
+# @doc.summary("新增学生信息")  # API信息描述
+# @doc.tag("学生")  # API分组
+# @doc.consumes({"product": {"stu_name": str, "age": int, "city": str}}, location="body")
+# async def add_student(request):
+#     return json({})
 
 
-@app.get("/class", version=1)
-@doc.summary("获取班级信息")  # API信息描述
-@doc.tag("班级")  # API分组
-@doc.consumes({"class_name": str}, location="query", required=False)
-@doc.consumes({"id": int}, location="query", required=True)
-async def get_class(request):
-    return json({})
+from sanic import Sanic
+from sanic.response import json
+from sanic_openapi import openapi2_blueprint
+
+app = Sanic(name="AwesomeApi")
+app.blueprint(openapi2_blueprint)
 
 
-@app.get("/studentList", version=1)
-@doc.summary("获取学生信息")
-@doc.tag("学生")
-@doc.consumes({"stu_name": str}, location="query", required=True)
-async def get_student(request):
-    return json({})
-
-
-@app.post("/addStudent", version=2)
-@doc.summary("新增学生信息")  # API信息描述
-@doc.tag("学生")  # API分组
-@doc.consumes({"product": {"stu_name": str, "age": int, "city": str}}, location="body")
-async def add_student(request):
-    return json({})
+@app.route("/")
+async def test(request):
+    return json({"hello": "world"})
 
 
 if __name__ == '__main__':
