0fc081f2ff567b8376b816d0dd243cd6333a92c3
zollty <zollty@163.com>
2024-03-11 21:08:01 +0800

    update

--- common_tests/sanic_test.py
+++ common_tests/sanic_test.py
@@ -5,10 +5,11 @@ from sanic.response import text as sanic_text
 from sanic_ext import openapi
 import json
 
-app = Sanic(name="sdjkjdhszollty")
 
 
 from typing import Any, List, Optional, Dict
+
+
 # from langchain.docstore.document import Document
 # from pydantic import BaseModel, Field
 
@@ -42,42 +43,42 @@ def testpyd() -> List[DocumentWithVSId]:
             DocumentWithVSId(page_content="yyyyyyyy", id="yyy", name="sdds", score=2.1)]
     return data
 
-@app.route("/testpyd")
-async def test(request):
-    return sanic_json(json.dumps(testpyd(), ensure_ascii=False)) # , default=lambda k: k.__dict__
 
+def mount_app(app):
+    @app.route("/testpyd")
+    async def test(request):
+        return sanic_json(json.dumps(testpyd(), ensure_ascii=False))  # , default=lambda k: k.__dict__
 
-@app.route("/")
-async def test(request):
-    return sanic_json({"hello": "world"})
+    @app.route("/")
+    async def test(request):
+        return sanic_json({"hello": "world"})
 
+    @app.get("/class", version=1)
+    @openapi.summary("获取班级信息")  # API信息描述
+    @openapi.tag("班级")  # API分组
+    @openapi.parameter({"class_name": str}, location="query", required=False)
+    @openapi.parameter({"id": int}, location="query", required=True)
+    async def get_class(request):
+        return sanic_json({})
 
-@app.get("/class", version=1)
-@openapi.summary("获取班级信息")  # API信息描述
-@openapi.tag("班级")  # API分组
-@openapi.parameter({"class_name": str}, location="query", required=False)
-@openapi.parameter({"id": int}, location="query", required=True)
-async def get_class(request):
-    return sanic_json({})
+    @app.get("/studentList", version=1)
+    @openapi.summary("获取学生信息")
+    @openapi.tag("学生")
+    @openapi.parameter({"stu_name": str}, location="query", required=True)
+    async def get_student(request):
+        return sanic_json({})
 
-
-@app.get("/studentList", version=1)
-@openapi.summary("获取学生信息")
-@openapi.tag("学生")
-@openapi.parameter({"stu_name": str}, location="query", required=True)
-async def get_student(request):
-    return sanic_json({})
-
-
-@app.post("/addStudent", version=2)
-@openapi.summary("新增学生信息")  # API信息描述
-@openapi.tag("学生")  # API分组
-@openapi.parameter({"product": {"stu_name": str, "age": int, "city": str}}, location="body")
-async def add_student(request):
-    return sanic_json({})
+    @app.post("/addStudent", version=2)
+    @openapi.summary("新增学生信息")  # API信息描述
+    @openapi.tag("学生")  # API分组
+    @openapi.parameter({"product": {"stu_name": str, "age": int, "city": str}}, location="body")
+    async def add_student(request):
+        return sanic_json({})
 
 
 if __name__ == '__main__':
+    app = Sanic(name="sdjkjdhszollty")
     app.config.API_VERSION = 'v0.0.1'
     app.config.API_TITLE = '异步平台 API文档'
+    mount_app(app)
     app.run(host="0.0.0.0", port=7500, debug=True)
