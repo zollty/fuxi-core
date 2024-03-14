4a86572e2cb5a899e9cd6a4bfa2b951a7583c8d2
zollty <zollty@163.com>
2024-03-14 12:24:45 +0800

    update

--- retrieval/chroma_test.py
+++ retrieval/chroma_test.py
@@ -5,16 +5,16 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 import chardet
 
-
 # 读取原始文档
-#raw_documents_sanguo = TextLoader('/ai/apps/Fuxi-Chatchat/knowledge_base/yby_bgelargezh/content/园博园参考资料.txt', encoding='utf-8').load()
-#raw_documents_xiyou = TextLoader('/ai/apps/Fuxi-Chatchat/knowledge_base/yby_bgelargezh/content/园博园介绍.txt', encoding='utf-8').load()
-#raw_documents_fw = TextLoader('/ai/apps/Fuxi-Chatchat/knowledge_base/yby_bgelargezh/content/园博园服务.txt', encoding='utf-8').load()
+# raw_documents_sanguo = TextLoader('/ai/apps/Fuxi-Chatchat/knowledge_base/yby_bgelargezh/content/园博园参考资料.txt', encoding='utf-8').load()
+# raw_documents_xiyou = TextLoader('/ai/apps/Fuxi-Chatchat/knowledge_base/yby_bgelargezh/content/园博园介绍.txt', encoding='utf-8').load()
+# raw_documents_fw = TextLoader('/ai/apps/Fuxi-Chatchat/knowledge_base/yby_bgelargezh/content/园博园服务.txt', encoding='utf-8').load()
 raw_documents_sanguo = TextLoader('/ai/apps/data/园博园参考资料.txt', encoding='utf-8').load()
 raw_documents_xiyou = TextLoader('/ai/apps/data/园博园介绍.txt', encoding='utf-8').load()
 raw_documents_fw = TextLoader('/ai/apps/data/园博园服务.txt', encoding='utf-8').load()
 
-print(len(raw_documents_sanguo[0].page_content + raw_documents_xiyou[0].page_content + raw_documents_fw[0].page_content))
+print(
+    len(raw_documents_sanguo[0].page_content + raw_documents_xiyou[0].page_content + raw_documents_fw[0].page_content))
 
 # 分割文档
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
@@ -26,12 +26,12 @@ print("documents nums:", documents.__len__())
 
 # 生成向量（embedding）
 # model_id = "/ai/models/nlp_corom_sentence-embedding_chinese-base"
-#model_id = "/ai/models/nlp_gte_sentence-embedding_chinese-large"
+# model_id = "/ai/models/nlp_gte_sentence-embedding_chinese-large"
 model_id = "/ai/models/bce-embedding-base_v1"
 # embeddings = ModelScopeEmbeddings(model_id=model_id)
 model_name = "/ai/models/BAAI_bge-large-zh-v1.5"
 model_kwargs = {'device': 'cuda'}
-encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
+encode_kwargs = {'normalize_embeddings': True}  # set True to compute cosine similarity
 embeddings1 = HuggingFaceBgeEmbeddings(
     model_name=model_name,
     model_kwargs=model_kwargs,
@@ -39,23 +39,29 @@ embeddings1 = HuggingFaceBgeEmbeddings(
     query_instruction="为这个句子生成表示以用于检索相关文章："
 )
 embeddings = HuggingFaceBgeEmbeddings(
-    model_name= model_name, #"/ai/models/bce-embedding-base_v1",# "/ai/models/moka-ai_m3e-large", # "/ai/models/BAAI_bge-reranker-large", # "/ai/models/text2vec-bge-large-chinese",
+    model_name=model_name,
+    # "/ai/models/bce-embedding-base_v1",# "/ai/models/moka-ai_m3e-large", # "/ai/models/BAAI_bge-reranker-large", # "/ai/models/text2vec-bge-large-chinese",
     model_kwargs=model_kwargs,
     encode_kwargs=encode_kwargs
 )
 db = Chroma.from_documents(documents, embedding=embeddings)
 
-# 检索
-query1 = "白蛇娘子"
-query2 = "美国"
-query3 = "照壁"
-query = "博纳"
-query4 = "梅苑山庄"
-query5 = "院融景园"
-docs = db.similarity_search(query, k=5)
+if __name__ == '__main__':
 
-# 打印结果
-for doc in docs:
-    print("===================================================")
-    print("metadata:", doc.metadata)
-    print("page_content:", doc.page_content)
+    sentences = [
+        "白蛇娘子",
+        "美国",
+        "照壁",
+        "博纳",
+        "梅苑山庄",
+        "院融景园",
+        "在景点分布布局上，主要的景点景区中有梅林山庄吗",
+        "介绍一下景点分布布局上的主要景点景区",
+    ]
+    for query in sentences:
+        print(f"\n\n\n\n-------------------------query: {query}")
+        docs = db.similarity_search(query, k=3)
+        for doc in docs:
+            print("===================================================")
+            print("metadata:", doc.metadata)
+            print("page_content:", doc.page_content)
