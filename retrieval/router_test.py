64ad3f23f75b5dd42574a3c4a17f4337d2d57dfe
zollty <zollty@163.com>
2024-03-14 10:17:07 +0800

    update

---
+++ retrieval/router_test.py
@@ -0,0 +1,44 @@
+from streamlit_router import StreamlitRouter
+import streamlit as st
+
+def index(router):
+	st.text("INDEXXX  fron page index")
+	x = st.number_input("task id")
+	if st.button("create task"):
+		router.redirect(*router.build("create_task", {"x": x}))
+	if st.button("cancel task"):
+		router.redirect(*router.build("cancel_task", {"x": x}))
+	if st.button("view task"):
+		router.redirect(*router.build("view_task", {"x": x}))
+	st.text("others on page index")
+
+# variable router auto inject if as first params
+def cancel_task(router, x):
+	st.text(f"DELETE  fron page cancel task x={x}")
+	if st.button("back to index"):
+		router.redirect(*router.build("index"))
+	st.text("others on page cancel task")
+
+# variable router auto inject if as first params
+def create_task(x, router):
+	st.text(f"POSTTTTT fron page create task x={x}")
+	if st.button("back to index"):
+		router.redirect(*router.build("index"))
+	st.text("others on page create task")
+
+
+router = StreamlitRouter()
+router.register(index, '/')
+router.register(cancel_task, "/tasks/<int:x>", methods=['DELETE'])
+router.register(create_task, "/tasks/<int:x>", methods=['POST'])
+
+
+# deco also works
+@router.map("/tasks/<int:x>")
+def view_task(x):
+	st.text(f"GETTTT fron page view task x={x}")
+	if st.button("back to index 2"):
+		router.redirect(*router.build("index"))
+	st.text("others on page view task")
+
+router.serve()
--- retrieval/test.py
+++ retrieval/test.py
@@ -1,44 +1,54 @@
-from streamlit_router import StreamlitRouter
-import streamlit as st
+from langchain.document_loaders import TextLoader
+from langchain.embeddings import ModelScopeEmbeddings
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+import chardet
+from .redis_search import create_and_run_index, insert_doc, retrieve_docs
 
-def index(router):
-	st.text("INDEXXX  fron page index")
-	x = st.number_input("task id")
-	if st.button("create task"):
-		router.redirect(*router.build("create_task", {"x": x}))
-	if st.button("cancel task"):
-		router.redirect(*router.build("cancel_task", {"x": x}))
-	if st.button("view task"):
-		router.redirect(*router.build("view_task", {"x": x}))
-	st.text("others on page index")
+# def read_file(path, encoding):
+#     result = []
+#     with open(path, 'r', encoding=encoding) as f:
+#         result.append(f.read())
+#     return result
 
-# variable router auto inject if as first params
-def cancel_task(router, x):
-	st.text(f"DELETE  fron page cancel task x={x}")
-	if st.button("back to index"):
-		router.redirect(*router.build("index"))
-	st.text("others on page cancel task")
+# 读取原始文档
+raw_documents_sanguo = TextLoader('/ai/apps/data/园博园参考资料.txt', encoding='utf-8').load()
+raw_documents_xiyou = TextLoader('/ai/apps/data/园博园介绍.txt', encoding='utf-8').load()
+raw_documents_fw = TextLoader('/ai/apps/data/园博园服务.txt', encoding='utf-8').load()
 
-# variable router auto inject if as first params
-def create_task(x, router):
-	st.text(f"POSTTTTT fron page create task x={x}")
-	if st.button("back to index"):
-		router.redirect(*router.build("index"))
-	st.text("others on page create task")
+print(
+    len(raw_documents_sanguo[0].page_content + raw_documents_xiyou[0].page_content + raw_documents_fw[0].page_content))
 
+# 分割文档
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+documents_sanguo = text_splitter.split_documents(raw_documents_sanguo)
+documents_xiyou = text_splitter.split_documents(raw_documents_xiyou)
+documents_fw = text_splitter.split_documents(raw_documents_fw)
+documents = documents_sanguo + documents_xiyou + documents_fw
+print("documents nums:", documents.__len__())
+print(documents[0].page_content)
+print(documents[:-1].page_content)
 
-router = StreamlitRouter()
-router.register(index, '/')
-router.register(cancel_task, "/tasks/<int:x>", methods=['DELETE'])
-router.register(create_task, "/tasks/<int:x>", methods=['POST'])
+if __name__ == '__main__':
 
+    kb_name = "yby"
+    create_and_run_index(kb_name)
 
-# deco also works
-@router.map("/tasks/<int:x>")
-def view_task(x):
-	st.text(f"GETTTT fron page view task x={x}")
-	if st.button("back to index 2"):
-		router.redirect(*router.build("index"))
-	st.text("others on page view task")
+    docs = [x.page_content for x in documents]
 
-router.serve()
+    insert_doc(docs, kb_name)
+
+    sentences = [
+        "白蛇娘子",
+        "美国",
+        "照壁",
+        "博纳",
+        "梅苑山庄",
+        "院融景园",
+    ]
+    for doc in sentences:
+        print(f"-------------------------query: {doc}")
+        results = retrieve_docs(doc, kb_name)
+        for x in results:
+            print(x["doc"], x["vector_distance"])
