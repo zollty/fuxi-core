169f511bee042b228bde5cbb3103a16fa3279007
zollty <zollty@163.com>
2024-03-19 22:03:11 +0800

    update

--- llm_model/controller.py
+++ llm_model/controller.py
@@ -187,7 +187,7 @@ def mount_controller_routes(app: FastAPI,
         print("--------------------init_get_registed_workers_info--------------------------")
         print(app._controller.worker_info)
         for w_name, w_info in app._controller.worker_info.items():
-            port = w_name.split(":")[1]
+            port = w_name.split(":")[2]
             for m in w_info.model_names:
                 global_worker_dict[m] = {"port": port, "success": True}
         print("--------------------global_worker_dict--------------------------")
